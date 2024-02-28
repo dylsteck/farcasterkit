705107b3e78c2376e3be8d288bfec3ab7f816c79
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2024-02-28 17:11:15 +0530

    replace template with `tsdx`

--- apps/web/README.md
+++
@@ -1,2 +0,0 @@
-# web
-The homepage and docs for Farcaster Kit. [View here](https://farcasterkit.com)
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  console.log('thing');
+  return <div>the snozzberries taste like snozzberries</div>;
+};
